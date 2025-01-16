a585c52a98e3eeb1433fc2b5feec95c039eac8cf
Nithya-Alfonisa <nithsnisha@gmail.com>
2025-01-16 21:46:28 +0530

    Update number_is_positiveornegative.py

--- basic-programs/number_is_positiveornegative.py
+++ basic-programs/number_is_positiveornegative.py
@@ -1,8 +1,8 @@
 print("To check whether the entered value is Positive, Negative or Zero")
 a = float(input("Enter a number"));
-if(a>0):
+if(a<0):
     print("The entered number is Negative")
-else if(a<0):
+elif(a>0):
     print("The entered number is Positive")
 else:
     print("The entered number is Zero")
