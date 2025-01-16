bb02c7e3c4587b08c540472328b682356f8b5358
Nithya-Alfonisa <nithsnisha@gmail.com>
2025-01-16 20:35:55 +0530

    Add files via upload

---
+++ basic-programs/inputstatement.py
@@ -0,0 +1,14 @@
+x,y=input("enter two values:").split()
+print("x value:",x)
+print("y value:",y)
+add=x+y
+print("add:",add)
+print(type(x))
+
+x=int(x)
+y=int(y)
+print(x)
+print(y)
+sum=x+y
+print ("sum:",sum)
+print(type(x))
