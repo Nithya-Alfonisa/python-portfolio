1bf6e703de18eb481e6bc860707de0df8287d5eb
Nithya-Alfonisa <nithsnisha@gmail.com>
2025-01-16 20:50:53 +0530

    Add files via upload

---
+++ basic-programs/Add_two_numbers.py
@@ -0,0 +1,5 @@
+#Adding of two numbers
+Number1 = float(input("enter first number:"))
+Number2 = float(input("enter second number:"))
+sum = number1 + number2
+print("The Addition of two numbers is:",sum)
